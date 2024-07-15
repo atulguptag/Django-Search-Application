2ae9d746ed7167cb9ea0544a827ce0d2344f42fb
Atul Gupta <atulguptag111@gmail.com>
2024-07-15 21:00:00 +0530

    Added .distinct() to remove duplicate results

--- restaurant_search/restaurant/views.py
+++ restaurant_search/restaurant/views.py
@@ -11,7 +11,8 @@ from .models import Dish
 def search(request):
     query = request.GET.get('q', '')
     if query:
-        dishes = Dish.objects.filter(Q(name__icontains=query) | Q(name__istartswith=query))[:10]
+        dishes = Dish.objects.filter(Q(name__icontains=query) | Q(
+            name__istartswith=query)).distinct()[:10]
     else:
         dishes = Dish.objects.none()
     context = {'query': query, 'dishes': dishes}
@@ -21,16 +22,15 @@ def search(request):
 @api_view(['GET', 'POST'])
 def restaurant_list(request):
     if request.method == 'GET':
-      restaurant = Dish.objects.all()
-      serializer = RestaurantSerializer(restaurant, many=True)
-      return Response(serializer.data)
+        restaurant = Dish.objects.all()
+        serializer = RestaurantSerializer(restaurant, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-      serializer = RestaurantSerializer(data=request.data)
-      serializer.is_valid(raise_exception=True)
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+        serializer = RestaurantSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -38,7 +38,7 @@ def restaurant_detail(request, pk):
     restaurants = get_object_or_404(Dish, pk=pk)
 
     if request.method == 'GET':
-        serializer = RestaurantSerializer(restaurants);
+        serializer = RestaurantSerializer(restaurants)
         return Response(serializer.data)
 
     elif request.method == "PUT":
@@ -46,9 +46,7 @@ def restaurant_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-        
+
     elif request.method == 'DELETE':
         restaurants.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
